68353be3da2322888993157a71ff577cfe52fdc9
goldbutnew <149566915+goldbutnew@users.noreply.github.com>
2024-02-07 19:25:40 +0900

    [Silver V] Title: 나이순 정렬, Time: 352 ms, Memory: 132484 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+lst = []
+
+for _ in range(t):
+    age, name = input().split()
+    age = int(age)
+    lst.append([age, name])
+
+lst.sort(key = lambda x : x[0])
+
+for i in lst:
+    print(i[0], i[1])
