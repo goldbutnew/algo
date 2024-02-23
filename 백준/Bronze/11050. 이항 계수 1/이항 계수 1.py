c8d8cf55a2df9d4c8865a1e739c65c78450f8c67
goldbutnew <149566915+goldbutnew@users.noreply.github.com>
2024-02-23 11:21:13 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+
+a = 1
+b = 1
+c = 1
+
+for i in range(1, n+1):
+    a *= i
+
+for i in range(1, k+1):
+    b *= i
+
+for i in range(1, (n-k)+1):
+    c *= i
+
+bc = b * c
+
+print(a // bc)
