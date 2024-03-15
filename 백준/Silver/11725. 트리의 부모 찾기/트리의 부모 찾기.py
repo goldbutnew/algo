ff0dc6a434210233dd82ac6595a0c41860cc0ea4
goldbutnew <149566915+goldbutnew@users.noreply.github.com>
2024-03-16 06:54:23 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 256 ms, Memory: 123624 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -8,21 +8,23 @@ for i in range(n - 1):
     tree[b].append(a)
 
 # 방문 체크
-visited = [0] * (n + 1)
+v = [0] * (n + 1)
 
 def dfs(tree, root):
     stack = [root]
     while stack:
         top = stack.pop()
         for adj in tree[top]:
-            if visited[adj] == 0:  # 방문한 적이 없다면
-                visited[adj] = top  # 해당 노드를 부모 노드로 저장
+            # 방문한 적 없는 노드를 v에 부모로 저장
+            if v[adj] == 0:
+                v[adj] = top
                 stack.append(adj)
-    return visited
+    return v
 
 
 # 트리의 루트는 언제나 1
 dfs(tree, 1)
 
-for x in range(2, n + 1):
-    print(visited[x])  # 각 인덱스(노드)에 저장된 부모 노드 가져오기
+# 각 노드에 저장된 부모 노드 가져오기
+for p in range(2, n + 1):
+    print(v[p])  
