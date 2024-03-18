5f6f82c8b8dcd9ea5831db23b9b7213ea6f69f5d
goldbutnew <149566915+goldbutnew@users.noreply.github.com>
2024-03-18 12:22:16 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+pitch = list(map(int, input().split()))
+
+asc = [1, 2, 3, 4, 5, 6, 7, 8]
+des = [8, 7, 6, 5, 4, 3, 2, 1]
+
+if pitch == asc:
+    print("ascending")
+elif pitch == des:
+    print("descending")
+else:
+    print("mixed")
